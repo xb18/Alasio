8b4cb3e7eaaf0773661e7c30fa90c4f115474eed
LmeSzinc <37934724+LmeSzinc@users.noreply.github.com>
2026-08-23 03:08:28 +0800

    Dep(webapp): shadcn-svelte Popover

---
+++ webapp/renderer/lib/components/ui/popover/popover-content.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { cn } from "$lib/utils.js";
+  import { Popover as PopoverPrimitive } from "bits-ui";
+
+  let {
+    ref = $bindable(null),
+    class: className,
+    sideOffset = 4,
+    align = "center",
+    portalProps,
+    ...restProps
+  }: PopoverPrimitive.ContentProps & {
+    portalProps?: PopoverPrimitive.PortalProps;
+  } = $props();
+</script>
+
+<!-- MODIFIED: remove animation -->
+<PopoverPrimitive.Portal {...portalProps}>
+  <PopoverPrimitive.Content
+    bind:ref
+    data-slot="popover-content"
+    {sideOffset}
+    {align}
+    class={cn(
+      "bg-popover text-popover-foreground data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 origin-(--bits-popover-content-transform-origin) outline-hidden z-50 w-72 rounded-md border p-4 shadow-md",
+      className,
+    )}
+    {...restProps}
+  />
+</PopoverPrimitive.Portal>
